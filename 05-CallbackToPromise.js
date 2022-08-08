ffca808d4de3bf3aae8d42de55347dce4751fb74
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-08 18:43:06 +0530

    Conversion of Callback fn to Promise

---
+++ 05-CallbackToPromise.js
@@ -0,0 +1,21 @@
+function setuser(id, name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ id, userName: name });
+    }, 2000);
+  });
+}
+
+function gethobbies(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username == "Ankrish") {
+        resolve(["Gaming", "Listning", "Running", "Swimming"]);
+      } else resolve(`You are not Ankrish`);
+    }, 2000);
+  });
+}
+
+setuser(10, "Ankrish").then((user) =>
+  gethobbies(user.userName).then((hobbies) => console.log(hobbies))
+);
